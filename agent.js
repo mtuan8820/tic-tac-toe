e392587265a94a9cef12267c26ae2425c96cc557
mtuan8820 <114607104+mtuan8820@users.noreply.github.com>
2024-06-05 17:50:03 +0700

    add heuristic function

--- agent.js
+++ agent.js
@@ -29,12 +29,52 @@ function isTerminalNode(node){
     return False
 }
 
+function countOccurrences(array){
+    let numX = 0, numO = 0, numNULL = 0
+    array.forEach(item =>{
+        if (item == 'X') numX++;
+        else if (item == 'O') numO++;
+        else numNULL++;
+    })
+    return [numX, numO, numNULL]
+}
+
 function caculateHeuristicVal(node){
-    return 0;
+    
+    let val = 0
+    function updateVal(array){
+        countOccur = countOccurrences(array)
+        let numX = countOccur[0]
+        let numO = countOccur[1]
+        let numNULL = countOccur[2]
+        if (numX == 3) val-=100
+        else if (numO == 3) val+=100
+        else if ((numX == 2) && numNULL == 1) val-=10
+        else if ((numO == 2) && numNULL == 1) val+=10
+        else if ((numX == 1) && numNULL == 2) val-=1
+    }
+    // check column
+    for (let i = 0; i < 3; i++){
+        let array = [node.board[i], node.board[i+3], node.board[i+6]]
+        updateVal(array)
+    }
+    // check row
+    for (let i = 0; i<7; i += 3){
+        let array = [node.board[i], node.board[i+1], node.board[i+2]]
+        updateVal(array)
+    }
+
+    let diagonal1 = [node.board[0], node.board[4], node.board[8]]
+    updateVal(diagonal1)
+    let diagonal2 = [node.board[2], node.board[4], node.board[6]]
+    updateVal(diagonal2)
+
+    return val
 }
 
 function minimax(node, depth, maximizingPlayer){
-    if (depth ==0 ) or (isTerminalNode(node))
+    
+    if ((depth ==0 ) || (isTerminalNode(node)))
         return caculateHeuristicVal(node)
     if (maximizingPlayer){
         let value = Number.NEGATIVE_INFINITY 
@@ -64,5 +104,3 @@ generateTree(root, 2)
 console.log(root.children)
 let copyroot = root
 copyroot.maximizingPlayer = false
-let a = document.getElementById('btn1')
-a.innerHTML = root.maximizingPlayer 
